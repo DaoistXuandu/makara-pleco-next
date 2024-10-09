1d0ef18d09cee877b1b2484f8a6e34cc84d85023
DaoistXuandu <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>
2024-10-09 17:25:46 +0700

    test socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,55 @@ import PositionLog from './components/positionLog';
 import SurfaceImaging from './components/surfaceImaging';
 import UnderwaterImaging from './components/underImage';
 import Position from './components/position';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const noto_sans = Noto_Sans({ subsets: ['latin'] })
 
 export default function Home() {
   const [track, setTrack] = useState(0)
+  const Ably = require('ably');
+  const [data, setData] = useState("")
+  const [value, setValue] = useState("")
+
+  async function publishSubscribe() {
+
+    // Connect to Ably with your API key
+    const ably = new Ably.Realtime(process.env.NEXT_PUBLIC_KEY)
+    ably.connection.once("connected", () => {
+      console.log("Connected to Ably!")
+    })
+
+    // Create a channel called 'get-started' and register a listener to subscribe to all messages with the name 'first'
+    const channel = ably.channels.get("get-started")
+    await channel.subscribe("first", (message: any) => {
+      console.log(message.data)
+      // setData(message.data)
+    });
+
+    // // Publish a message with the name 'first' and the contents 'Here is my first message!'
+    // await channel.publish("first", msg)
+
+    // // Close the connection to Ably after a 5 second delay
+    // setTimeout(async () => {
+    //   ably.connection.close();
+    //   await ably.connection.once("closed", function () {
+    //     console.log("Closed the connection to Ably.")
+    //   });
+    // }, 5000);
+  }
+
+  useEffect(() => {
+    publishSubscribe()
+  }, [])
+
+
 
   return (
     <div className={`flex text-black flex-col space-y-5 p-12 bg-white h-screen ${noto_sans.className}`}>
       <Title />
+      <div className='h-40 w-full'>
+        {(data == "" ? "Null" : data)}
+      </div>
+      <input className='w-full h-fit border-1 border-green-200' type="text" onChange={e => setValue(e.target.value)} />
       <div className='flex flex-row'>
         <div className='flex flex-col w-1/2 space-y-3'>
           <GeoTag />
